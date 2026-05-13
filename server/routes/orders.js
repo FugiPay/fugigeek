39c972adbb5bec1e25e83ba8d4713cc232981f34
FugiPay <khahtech2010@gmail.com>
2026-05-13 11:45:23 +0200

    fix: lazy init S3, fix orders routes

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router  = express.Router();
-const { protect, authorize } = require('../middleware/auth');
-const { createOrder, getOrders, getOrder, submitOrder, completeOrder, disputeOrder } = require('../controllers/ordersController');
+const { protect } = require('../middleware/auth');
+const {
+  createOrder, getOrders, getOrder,
+  submitOrder, verifyOrder, disputeOrder, withdrawOrder,
+} = require('../controllers/ordersController');
 
-router.post('/',               protect, authorize('business'),      createOrder);
-router.get( '/',               protect,                             getOrders);
-router.get( '/:id',            protect,                             getOrder);
-router.put( '/:id/submit',     protect, authorize('professional'),  submitOrder);
-router.put( '/:id/complete',   protect, authorize('business'),      completeOrder);
-router.put( '/:id/dispute',    protect,                             disputeOrder);
+router.post('/',               protect, createOrder);
+router.get( '/',               protect, getOrders);
+router.get( '/:id',            protect, getOrder);
+router.put( '/:id/submit',     protect, submitOrder);
+router.put( '/:id/verify',     protect, verifyOrder);
+router.put( '/:id/dispute',    protect, disputeOrder);
+router.put( '/:id/withdraw',   protect, withdrawOrder);
 
 module.exports = router;
